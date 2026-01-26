ccbb9d2773155314940d67aff52c7624507b1ab5
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2026-01-26 20:53:27 +0100

    Improve texture return on render end

--- RealTimeMenus/internal/Game/Bethesda/BSShaderManager.hpp
+++ RealTimeMenus/internal/Game/Bethesda/BSShaderManager.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include "BSTextureManager.hpp"
+
 class ShadowSceneNode;
 class BSRenderedTexture;
 class NiDX9Renderer;
 
 class BSShaderManager {
 public:
+	static BSTextureManager* GetTextureManager();
+
 	static ShadowSceneNode* GetShadowSceneNode(uint32_t aeType);
 
 	static void RenderImageSpaceEffects(NiDX9Renderer* apRenderer, BSRenderedTexture* apSourceTarget, BSRenderedTexture* apDestTarget);
